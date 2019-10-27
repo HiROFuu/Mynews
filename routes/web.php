38aec0d311a524f25797dd3d153383f268bfcfdf
名前 <>
2019-10-27 13:59:05 +0000

    11回課題

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,6 @@ Route::get('/', function () {
 });
 Route::group(['prefix' => 'admin'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
-});
-
-Route::get('XXX', 'Admin\AAAController@bbb');
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('admin/profile/create', 'Admin\ProfileController@add');
-});
-
-Route::group(['prefix' => 'admin'], function() {
-    Route::get('admin/profile/edit', 'Admin\ProfileController@edit');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit', 'Admin\ProfileController@edit');
 });
