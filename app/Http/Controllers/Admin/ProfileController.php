387b677c66267ecd24b70ed8482cc078098ec9b7
名前 <>
2019-11-13 13:04:52 +0000

    課題14提出

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Profile;
 
 class ProfileController extends Controller
 {
@@ -12,19 +13,34 @@ class ProfileController extends Controller
     {
         return view('admin.profile.create');
     }
+    public function create(Request $request)
+  {
+      // 以下を追記
+      // Varidationを行う
+      $this->validate($request, Profile::$rules);
 
-    public function create()
-    {
-        return redirect('admin/profile/create');
-    }
+      $profile = new Profile;
+      $form = $request->all();
 
-    public function edit()
-    {
-        return view('admin.profile.edit');
-    }
+      // フォームから画像が送信されてきたら、保存して、$news->image_path に画像のパスを保存する
+      if (isset($form['image'])) {
+        $path = $request->file('image')->store('public/image');
+        $profile->image_path = basename($path);
+      } else {
+          $profile->image_path = null;
+      }
 
-    public function update()
-    {
-        return redirect('admin/profile/edit');
+      // フォームから送信されてきた_tokenを削除する
+      unset($form['_token']);
+      // フォームから送信されてきたimageを削除する
+      unset($form['image']);
+      
+           // データベースに保存する
+      $profile->fill($form);
+      $profile->save();
+      
+      // admin/news/createにリダイレクトする
+      return redirect('admin/profile/create');
+
     }
 }
